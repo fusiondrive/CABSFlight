c831b4e2f9796dec888764fd2e1812d86c6e1a1c
fusiondrive <473072010@qq.com>
2026-02-07 12:58:06 -0500

    Initial Commit

---
+++ CABSFlight/CABSFlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  CABSFlightApp.swift
+//  CABSFlight
+//
+//  Created by Steve on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CABSFlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
